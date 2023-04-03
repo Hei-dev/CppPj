7d2b201deb8197063ea8530446a511de86f660d0
Hei-dev <59471608+Hei-dev@users.noreply.github.com>
2023-04-03 16:25:41 +0800

    Created main menu
    -Created the main menu
    -Added functions declarations for each functions

--- main.cpp
+++ main.cpp
@@ -3,11 +3,25 @@
 #include <iomanip>
 #include <string>
 #include <cstring>
+#include <filesystem>
 
 using namespace std;
 
+void memberList(){
+
+}
+
+//Variables declaration here
+CSVObject bookList;
+CSVObject borrowList;
+
+//Create class/objects here
 class CSVObject {
 	public :
+		static bool fileExist(string path){
+			FILE* _f = fopen(path.c_str(),"r");
+			return (_f!=NULL);
+		}
 		/**
 		 * Split a string with a string delimitor into an array
 		 * @param s - the string to be splitted
@@ -60,20 +74,25 @@ class CSVObject {
 		    }
 		}
 		
-		void readCSV(string fileLocation){
+		bool readCSV(string fileLocation){
+			if(!fileExist(fileLocation)){
+				return false;
+			}
 		    int dum;
-		    cout << fileLocation.c_str();
+		    //cout << fileLocation.c_str();
 		    //cin >> dum;
 			FILE* csvfiles=fopen(fileLocation.c_str(),"r");
+			if(csvfiles==NULL){return false;}
 			char line[255];
 			int i=0;
 			while (fgets(line, sizeof line, csvfiles) != NULL ){
 				//cout << line;
 				split(line,",",elements[i]);
 				i++;
-				cout<<i<<endl;
+				//cout<<i<<endl;
 			}
 			fclose(csvfiles);
+			return true;
 		}
 		
 	private:
@@ -81,57 +100,107 @@ class CSVObject {
 			
 };
 
-bool checkYNvalid(string ynchoice){
-    return (ynchoice=="Y"||ynchoice=="N");
+//Custom functions here
+bool checkYNvalid(char choice){
+    return (choice=='Y'||choice=='N');
 }
 
-bool checkYN(string ynchoice){
-    return (ynchoice=="Y");
+bool checkYN(char choice){
+    return (choice=='Y');
 }
 
-//Variables declaration here
-CSVObject bookList;
-CSVObject borrowList;
+//Main functions here
+void manageBooks(){
+
+}
+
+void manageBorrowers(){
+	
+}
+
+void borrowBook(){
+
+}
+
+void returnBook(){
+
+}
+
+void usefulFeaturesMenu(){
+
+}
 
 int main(){
-    string ynchoice;
+    char choice;
     //Check if user wants to import from file for book list
     do{
         cout << "Import book list from file? [Y/N]: ";
-        cin >> ynchoice;
-        if(checkYNvalid(ynchoice))
+        cin >> choice;
+        if(!checkYNvalid(choice)){
             cout << "Invalid input, please type again.\n";
-    }while(checkYNvalid(ynchoice));
-	if(checkYN(ynchoice)){
+		}
+    }while(!checkYNvalid(choice));
+	if(checkYN(choice)){
 	    string path;
 	    cout << "Path of book list file: ";
 	    cin >> path;
 	    cout << "Importing book list... ";
-	    bookList.readCSV(path);
-	    cout << "Done";
+	    if(bookList.readCSV(path))
+	    	cout << "Done\n";
+		else
+			cout << "Error while importing. Book list will not be imported.\n";
 	}
 	else{
-	    cout << "No book list is imported ";
+	    cout << "No book list is imported \n";
 	}
 	
 	//Check if user wants to import from file for borrow list
     do{
         cout << "Import borrower list from file? [Y/N]: ";
-        cin >> ynchoice;
-        if(checkYNvalid(ynchoice))
+        cin >> choice;
+        if(!checkYNvalid(choice))
             cout << "Invalid input, please type again.\n";
-    }while(checkYNvalid(ynchoice));
-	if(checkYN(ynchoice)){
+    }while(!checkYNvalid(choice));
+	if(checkYN(choice)){
 	    string path;
 	    cout << "Path of borrower list file: ";
 	    cin >> path;
 	    cout << "Importing borrower list... ";
-	    borrowList.readCSV(path);
-	    cout << "Done";
+	    if(borrowList.readCSV(path))
+	    	cout << "Done\n";
+		else
+			cout << "Error while importing. Borrower list will not be imported.\n";
 	}
 	else{
-	    cout << "No borrower list is imported ";
+	    cout << "No borrower list is imported \n";
 	}
 	
+	//MAIN MENU
+	do{
+		cout << "*** Library Management System ***" << endl;
+		cout << "[1] Manage books" << endl;
+		cout << "[2] Manage borrowers" << endl;
+		cout << "[3] Borrow book(s)" << endl;
+		cout << "[4] Return book(s)" << endl;
+		cout << "[5] Useful feature(s) added" << endl;
+		cout << "[6] Member List" << endl;
+		cout << "[7] Exit" << endl;
+		cout << "*********************************" << endl;
+		cout << "Option (1 - 7): " << endl;
+		cin >> choice;
+
+		switch (choice){
+			case '1': manageBooks(); break;
+			case '2': manageBorrowers(); break;
+			case '3': borrowBook(); break;
+			case '4': returnBook(); break;
+			case '5': usefulFeaturesMenu(); break;
+			case '6': memberList(); break;
+			case '7': /*Do nothing*/ break;
+		}
+	}while(choice!='7');
+
+	cout << "Exiting program. Goodbye!";
+
 	return 0;
 }
